a9280de455a7daa67f663ebf06f9cd084cdb122e
Jeremy Hummel <jmhummel@gmail.com>
2019-06-24 12:23:10 -0700

    update num epochs to 10

--- digits2.py
+++ digits2.py
@@ -51,7 +51,7 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # 9. Fit model on training data
-model.fit(X_train, Y_train, batch_size=32, nb_epoch=1, verbose=1)
+model.fit(X_train, Y_train, batch_size=32, nb_epoch=10, verbose=1)
 
 # 10. Evaluate model on test data
 score = model.evaluate(X_test, Y_test, verbose=1)
